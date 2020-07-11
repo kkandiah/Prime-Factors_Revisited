f16c6bde6418643bf621586e5f9b7e8abeee42ee
Kulendran Kandiah <kkandiah@hotmail.com>
2020-07-11 14:59:52 -0400

    Tutorial prime factors part 1 solved using iterator class

--- prime.py
+++ prime.py
@@ -7,9 +7,35 @@ class PrimeFactors:
     """
     docstring for Prime_Factors.
     """
-
-    def __init__(self, arg):
-        if not isinstance(arg, int):
-            self.arg = ValueError
+    def __init__(self, number):
+        if not isinstance(number, int):
+            self.number = ValueError
         else:
-            self.arg = arg
+            self.number = number
+
+    def __iter__(self):
+        """
+        Returns the instance object which is an iterator.
+        """
+        self.control = 0
+        return self
+
+    def __next__(self):
+        """
+        Defines the instance object as an iterator.
+        """
+        if self.number == 1 or self.control == 1:
+            raise StopIteration
+        i = 2
+        while i * i <= self.number:
+            if self.number % i:
+                i += 1
+            else:
+                self.number //= i
+                prime_factor = i
+                return prime_factor
+
+        if self.number > 1:
+            prime_factor = self.number
+            self.control = 1
+            return prime_factor
--- tests/test_prime.py
+++ tests/test_prime.py
@@ -11,4 +11,60 @@ def  test_valueerror():
     that is not an integer (e.g. a string or float), a ValueError is raised.
     """
     p_1 = prime.PrimeFactors('a')
-    assert p_1.arg == ValueError
+    assert p_1.number == ValueError
+
+def test_one():
+    """
+    asserts that when prime_factors is called with 1,
+    an empty list will be generated
+    """
+    p_1 = list(prime.PrimeFactors(1))
+    assert p_1 == []
+
+def test_two():
+    """
+    asserts that when prime_factors is called with 2,
+    the list [2] will be generated
+    """
+    p_1 = list(prime.PrimeFactors(2))
+    assert p_1 == [2]
+
+def test_three():
+    """
+    asserts that when prime_factors is called with 3,
+    the list [3] will be generated
+    """
+    p_1 = list(prime.PrimeFactors(3))
+    assert p_1 == [3]
+
+def test_four():
+    """
+    asserts that when prime_factors is called with 4,
+    the list [2, 2] will be generated
+    """
+    p_1 = list(prime.PrimeFactors(4))
+    assert p_1 == [2, 2]
+
+def test_six():
+    """
+    asserts that when prime_factors is called with 6,
+    the list [2, 3] will be generated
+    """
+    p_1 = list(prime.PrimeFactors(6))
+    assert p_1 == [2, 3]
+
+def test_eight():
+    """
+    asserts that when prime_factors is called with 8,
+    the list [2, 2, 2] will be generated
+    """
+    p_1 = list(prime.PrimeFactors(8))
+    assert p_1 == [2, 2, 2]
+
+def test_nine():
+    """
+    asserts that when prime_factors is called with 9,
+    the list [3, 3] will be generated
+    """
+    p_1 = list(prime.PrimeFactors(9))
+    assert p_1 == [3, 3]
